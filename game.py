f4907244d804c819cb391ccbd84ab400c5a48323
Atrashonok <436bgh151097@gmail.com>
2019-08-12 04:08:24 +0300

    random for KEY added, colors added

--- game.py
+++ game.py
@@ -5,7 +5,24 @@ import math
 import hmac
 import hashlib
 import binascii
+from termcolor import colored
 
+CYELLOW = '\33[33m'
+CEND = '\033[0m'
+
+def create_sha256_signature(key, message):
+    byte_key = binascii.unhexlify(key)
+    message = message.encode()
+    return hmac.new(byte_key, message, hashlib.sha256).hexdigest().upper()
+
+def check():
+    for i in range(1, len(sys.argv)):
+        if i == computerChoice:
+            print (CYELLOW,i, ": ", create_sha256_signature(str(preKey), str(i)), CEND)
+        else:
+            print (i, ": ", create_sha256_signature(str(preKey), str(i)))
+
+            
 numberOfArguments = len(sys.argv)
 gameSize = int((numberOfArguments-1) / 2)
 
@@ -22,19 +39,17 @@ elif numberOfArguments % 2 != 0:
 secretsGenerator = secrets.SystemRandom()
 computerChoice = secretsGenerator.randint(1,len(sys.argv)-1)
 
-def create_sha256_signature(key, message):
-    byte_key = binascii.unhexlify(key)
-    message = message.encode()
-    return hmac.new(byte_key, message, hashlib.sha256).hexdigest().upper()
+secretsGenerator = secrets.SystemRandom()
+preKey = secretsGenerator.randint(11111111111111111111111111111111,99999999999999999999999999999999)
 
-secretKey = create_sha256_signature("E49756B4C8FAB4E48222A3E7F3B97111", str(computerChoice))
+usedKey = create_sha256_signature(str(preKey), str(computerChoice))
 
-print("KEY: ", secretKey)
+print(CYELLOW, "KEY: ", usedKey, CEND)
 
 print("it's your turn to choose: ")
 for i in range(1, len(sys.argv)):
     print(i, ":  ", sys.argv[i])
-print ("0", ":  ", "---EXIT---")
+print ("0:", "---EXIT---")
 
 userChoice = int(input())
 
@@ -43,6 +58,7 @@ if userChoice == 0:
     sys.exit()
 elif (userChoice == computerChoice):
     print("DRAW")
+    check()
     sys.exit()
 else:  
     controlVariable = userChoice - computerChoice
@@ -50,17 +66,19 @@ else:
 if controlVariable < 0:
     if math.fabs(controlVariable) <= gameSize:
         print("YOU WON")
+        check()
         sys.exit()
     else:
          print("YOU LOSE")
+         check()
          sys.exit()
 elif controlVariable > 0:
     if (numberOfArguments-1) - controlVariable <= gameSize:
         print("YOU WON")
+        check()
         sys.exit()
     else:
         print("YOU LOSE")
+        check()
         sys.exit()
         
-
-    
